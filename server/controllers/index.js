017099eecf2c3caa7aafe72c04f69fe500a850ec
lingyc <lingchen215@gmail.com>
2016-07-01 18:22:06 -0700

    Get serve to post, setup mySQL database

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,8 +2,20 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+
+      res.send({});
+
+
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+
+      collectData(req, function(data) {
+        console.log(data);
+        sendResponse(res, data);
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
@@ -13,3 +25,28 @@ module.exports = {
   }
 };
 
+
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
+
+var sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
+
+var collectData = function(request, callback) {
+  var data = '';
+  request.on('data', function(chunk) {
+    console.log('allabout chunk:',typeof chunk,chunk);
+    data += chunk;
+  });
+  request.on('end', function() {
+    callback(JSON.parse(JSON.stringify(data)));
+  });
+};
--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,33 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE messages
+(
+Id int NOT NULL,
+text varchar(255) NOT NULL,
+time varchar(255) NOT NULL,
+user int NOT NULL, 
+room int NOT NULL,
+
+PRIMARY KEY (Id),
+FOREIGN KEY (user) REFERENCES users(Id),
+FOREIGN KEY (room) REFERENCES rooms(Id)
+
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users
+(
+Id int NOT NULL,
+name varchar(255) NOT NULL,
+PRIMARY KEY (Id)
+);
 
-
+CREATE TABLE rooms
+(
+Id int NOT NULL,
+name varchar(255) NOT NULL,
+PRIMARY KEY (Id)
+);
 
 
 /*  Execute this file from the command line by typing:
